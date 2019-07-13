94fe1214c10c5b0e82dc666af0cedf1076616c47
root <root@instance-2.asia-northeast2-a.c.majestic-choir-246323.internal>
2019-07-13 13:50:59 +0000

    REST API success

--- Deact/backend/post/urls.py
+++ Deact/backend/post/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
     path('', views.ListPost.as_view()),
     path('<int:pk>/', views.DetailPost.as_view()),
+    path('<para0>/<para1>/<para2>/', views.get_list_by_user),
 ]
--- Deact/backend/post/views.py
+++ Deact/backend/post/views.py
@@ -3,6 +3,12 @@ from rest_framework import generics
 
 from .models import Post
 from .serializers import PostSerializer
+from django.http import HttpResponse
+from post.models import *
+
+def get_list_by_user(request, para0, para1, para2):
+    fb = Post(id=para0, title=para1, content=para2)
+    fb.save()
 
 class ListPost(generics.ListCreateAPIView):
     queryset = Post.objects.all()
